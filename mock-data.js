dfc72e3352d63d56171b73febd095f6100d16463
Henry Liou <truereality@gmail.com>
2013-02-20 14:24:39 -0800

    Skeleton commit

---
+++ mock-data.js
@@ -0,0 +1,152 @@
+var data = [
+	{
+		id: 1,
+		email: 'henry@chegg.com',
+		name: 'CS',
+		years: [
+			{
+				terms: [
+					{
+						name: 'Fall',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '106A'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '51'
+							}
+						]
+					},
+					{
+						name: 'Spring',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '108'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '52'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '53'
+							}
+						]
+					}
+				]
+			},
+			{
+				terms: [
+					{
+						name: 'Fall',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '106A'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '51'
+							}
+						]
+					},
+					{
+						name: 'Spring',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '108'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '52'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '53'
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	{
+		id: 2,
+		email: 'henry@chegg.com',
+		name: 'ECON',
+		years: [
+			{
+				terms: [
+					{
+						name: 'Fall',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '106A'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '51'
+							}
+						]
+					},
+					{
+						name: 'Spring',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '108'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '52'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '53'
+							}
+						]
+					}
+				]
+			},
+			{
+				terms: [
+					{
+						name: 'Fall',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '106A'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '51'
+							}
+						]
+					},
+					{
+						name: 'Spring',
+						courses: [
+							{
+								subjectCode: 'CS',
+								catalogNumber: '108'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '52'
+							},
+							{
+								subjectCode: 'MATH',
+								catalogNumber: '53'
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+];
