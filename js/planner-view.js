1d881a1106e0200513db45146ad4c02e0ad59145
Henry Liou <truereality@gmail.com>
2013-02-20 21:36:09 -0800

    Add render to view

--- js/planner-view.js
+++ js/planner-view.js
@@ -1,13 +1,50 @@
 P('main');
-P.main.view = function(){
+P.main.view = function() {
 	return{
-		render: function(){
-			console.log(P.mainModel.getData());
-			// RENDER main widget now
+		render: function() {
+			console.log(P.data);
+			
+			var data = P.data;
+			var classyear = 2013;
+			
+			for(var plan in data) {
+				
+				plan = data[plan];
+				$('#tabs').append('<li>' + plan.name + '</li>');
+				
+				var planData = JSON.parse(plan.data);
+				
+				for(var year in planData.years) {
+					
+					var html = '<div class="year"><div class="year-label">' + (classyear - (planData.years.length - 1 - year)) + "</div>";
+					year = planData.years[year];
+					
+					for(var term in year.terms) {
+					
+						term = year.terms[term];
+						html += '<div class="term"><div class="term-label">' + term.name + '</div>';
+						
+						for(var course in term.courses) {
+						
+							course = term.courses[course];
+							html += '<div class="course">';
+							html += course.subjectCode + ' ' + course.catalogNumber;
+							html += '</div>';
+						}
+						
+						html += '</div>';
+					}
+					
+					html += '</div>';
+					
+					$('#years').append(html);
+				}
+			}
 		},
 		
 		load: function(){
 			P.mainModel = new P.main.model();
+			
 		//	P.mainModel.setData(P.data);
 		}
 	}
